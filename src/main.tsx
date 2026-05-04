2a6d2041a5926a3269e0bbb0603837c7a120bb1f
Arsalan Ahmed <116038959+arsalan-ahmed7790@users.noreply.github.com>
2026-05-05 02:03:20 +0500

    final: SPA build ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
-import "./styles.css";
 
-const router = getRouter();
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
-const rootElement = document.getElementById("root")!;
-
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
-  );
-}
+root.render(<h1>App Working</h1>);
